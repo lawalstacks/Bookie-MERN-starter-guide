b5757a1712c0d39929f987462b73fc5ad6d03791
Kazeem Lawal <quazeemastic@gmail.com>
2024-05-23 07:45:43 +0100

    connect database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require('express');
-
+const connectDB = require('./connectDB');
 
 const app  = express();
 const PORT = process.env.PORT || 8000;
+
+//middleware
+connectDB();
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.get('/',(req,res)=>{
 	res.json('hello lawalstacks.js!');
