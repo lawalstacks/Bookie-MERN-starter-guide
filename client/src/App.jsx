26b689b83160e3041fabe757d74a6c2675faab0b
Kazeem Lawal <quazeemastic@gmail.com>
2024-05-23 12:59:38 +0100

    setup client

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+
+
+function App() {
+
+  return (
+    <>
+	<Router>
+	  Header
+	  <Routes>
+	  Hello
+	  </Routes>
+	  Footer
+	  </Router>
+    </>
+  )
+}
+
+export default App
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require('express');
 const connectDB = require('./connectDB');
+const Book = require('./models/Books');
 
 const app  = express();
 const PORT = process.env.PORT || 8000;
@@ -11,6 +12,18 @@ connectDB();
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
+
+
+
+app.get('/api/books',async (req,res)=>{
+        try{
+		const data = await Book.find({});
+		res.json(data);
+	}catch(error){
+		res.status(500).json({error: 'An error occurred while fetching books.'});
+	}
+});
 
 app.get('/',(req,res)=>{
 	res.json('hello lawalstacks.js!');
