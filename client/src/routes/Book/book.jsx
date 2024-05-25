29840e7baf3c9f0e99d6f74ad576e3349ec3785e
Kazeem Lawal <quazeemastic@gmail.com>
2024-05-25 09:28:32 +0100

    Add book route and fetch data

---
+++ client/src/routes/Book/book.jsx
@@ -0,0 +1,15 @@
+import React,{useState,useEffect} from 'react';
+import { Link } from 'react-router-dom';
+
+function Book(){
+	return(
+		<div>
+		<h1>Books</h1>
+		<p>This is where we implement Nodejs,Express & MongoDB to grab some data. The data below is pulled from a MongoDB database</p>
+
+		<h2>Fetch Example<h2>
+		</div>
+	);
+}
+
+export default Book;
